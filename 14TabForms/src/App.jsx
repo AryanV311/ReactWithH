459c4f58a958fa048738b74f6e1aaf6506308414
AryanV311 <159629630+AryanV311@users.noreply.github.com>
2025-04-12 00:51:18 +0530

    added next prev button

--- 14TabForms/src/App.jsx
+++ 14TabForms/src/App.jsx
@@ -1,50 +1,85 @@
-import React, { useState } from 'react'
-import { Profile } from './components/Profile'
-import { Interets } from './components/Interets'
-import { Setting } from './components/Setting'
+import React, { useState } from "react";
+import { Profile } from "./components/Profile";
+import { Interets } from "./components/Interets";
+import { Setting } from "./components/Setting";
 
- const App = () => {
-
+const App = () => {
   const [data, setData] = useState({
-    name:'Aka',
-    email:"Aka1@gmail.com",
-    age:21,
-    interets:["coding", "jumping"],
-    theme:"dark"
-    })
-  const [activeTabs, setActiveTabs] = useState(0)
+    name: "Aka",
+    email: "Aka1@gmail.com",
+    age: 21,
+    interests: ["coding", "jumping"],
+    theme: "dark",
+  });
+  const [activeTabs, setActiveTabs] = useState(0);
 
   const tabs = [
     {
-      name:"Profile",
-      component:Profile
+      name: "Profile",
+      component: Profile,
     },
     {
-      name:"Interets",
-      component:Interets
+      name: "Interets",
+      component: Interets,
     },
     {
-      name:"Setting",
-      component:Setting
+      name: "Setting",
+      component: Setting,
     },
-  ]
+  ];
 
-  const ActiveTabContainer = tabs[activeTabs].component
+  const handlePrevChange = () => {
+    setActiveTabs((prev) => prev - 1);
+  };
+
+  const handleNextChange = () => {
+    setActiveTabs(prev => prev + 1)
+  };
+  const handleSubmit = () => {};
+
+  const ActiveTabContainer = tabs[activeTabs].component;
 
   return (
     <div>
-      <div className='tabs-container'>
-        {
-          tabs.map((tabs,index) => (
-            <div onClick={() => setActiveTabs(index)} key={index} className='tabs'>{tabs.name}</div>
-          ))
-        }
+      <div className="tabs-container">
+        {tabs.map((tabs, index) => (
+          <div
+            onClick={() => setActiveTabs(index)}
+            key={index}
+            className="tabs"
+          >
+            {tabs.name}
+          </div>
+        ))}
       </div>
-      <div className='active-tab'>
+      <div className="active-tab">
         <ActiveTabContainer data={data} setData={setData} />
       </div>
+
+      <div className="btn">
+        {activeTabs > 0 && (
+          <button onClick={handlePrevChange} className="prevBtn">
+            prev
+          </button>
+        )}
+
+{activeTabs < tabs.length - 1 && (
+          <button onClick={handleNextChange} className="prevBtn">
+            Next
+          </button>
+        )}
+        {
+          activeTabs === tabs.length -1 && <button onClick={handleSubmit} className="prevBtn">
+          Submit
+        </button>
+        }
+      </div>
+
+
+        
+      
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- 14TabForms/src/components/Interets.jsx
+++ 14TabForms/src/components/Interets.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 
 export const Interets = ({data, setData}) => {
-const {interets} = data
+const {interests} = data
 
+const handleInteretsChange = (e) => {
+    setData((prev) => (
+        {...prev, interests:e.target.checked ?
+        [...prev.interests, e.target.name] : prev.interests.filter((i) => i !== e.target.name)
+    }))
+}
+console.log(interests);
 
   return (
     <div>
          <div>
             <label>
-                <input type="checkbox" name='coding' checked={interets.includes("coding")} /> coding
+                <input type="checkbox" name='coding' checked={interests.includes("coding")} onChange={handleInteretsChange} /> coding
             </label>
         </div>
          <div>
             <label>
-                <input type="checkbox" name='writing' checked={interets.includes("writing")} /> writing
+                <input type="checkbox" name='writing' checked={interests.includes("writing")} onChange={handleInteretsChange} /> writing
             </label>
         </div>
          <div>
             <label>
-                <input type="checkbox" name='reading' checked={interets.includes("reading")} /> Reading
+                <input type="checkbox" name='reading' checked={interests.includes("reading")} onChange={handleInteretsChange} /> Reading
             </label>
         </div>
     </div>
