acdb6f3a890a393b006f965fe65365dfeca3f029
AryanV311 <159629630+AryanV311@users.noreply.github.com>
2025-04-13 00:33:58 +0530

    added next prev button

--- 15PaginationProblem/src/App.jsx
+++ 15PaginationProblem/src/App.jsx
@@ -4,6 +4,7 @@ import { ProductList } from "./components/ProductList";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const fetchProducts = async () => {
     try {
@@ -20,12 +21,36 @@ function App() {
     fetchProducts();
   }, []);
 
+  const PAGE_SIZE = 10;
+
+  const totalProducts = products.length;
+  const noOfPages = Math.ceil(totalProducts / PAGE_SIZE);
+  const start = currentPage * PAGE_SIZE;
+  const end = start + PAGE_SIZE
+
+  const handleNextPage = () => {
+    setCurrentPage((prev) => prev + 1 )
+  }
+  const handlePrevPage = () => {
+    setCurrentPage((prev) => prev - 1 )
+  }
+
+
   return (
     <>
       <div className="App">
         <h1>Pagination</h1>
+        <div className="pagination-container">
+          <button disabled={currentPage <= 0} className="prev-page" onClick={handlePrevPage}>◀️</button>
+          {
+            [...Array(noOfPages).keys()].map((n) =>(
+              <button key={n} onClick={() => setCurrentPage(n)} className={`pagination ${currentPage === n ? "active" : ""}`}>{n}</button>
+            ))
+          }
+          <button disabled={currentPage === noOfPages -1 } className="next-page" onClick={handleNextPage}>▶️</button>
+        </div>
         <div className="products-container">
-          {products.map((item) => ( 
+          {products.slice(start,end).map((item) => ( 
             <ProductList key={item.id} title={item.title} img={item.thumbnail} />
           ))}
         </div>
