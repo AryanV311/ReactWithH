25f8de00af8a10b5194bff832cc8b9696b4b421f
AryanV311 <159629630+AryanV311@users.noreply.github.com>
2025-05-16 14:28:47 +0530

    added search bar

--- 16AddToCartWithRedux/src/pages/Home.jsx
+++ 16AddToCartWithRedux/src/pages/Home.jsx
@@ -1,33 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import { ProductsCard } from '../components/ProductsCard'
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import { ProductsCard } from "../components/ProductsCard";
+import axios from "axios";
+import { CiSearch } from "react-icons/ci";
 
 export const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [input, setInput] = useState('');
+  const [cached, setcached] = useState({});
 
-    const [products, setProducts] = useState([]);
+  const fetchData = async () => {
 
-    const fetchData = async() => {
-      const data = await axios.get('https://dummyjson.com/products')
-      
-      setProducts(data.data.products)
+    if(cached[input]){
+      console.log("Cached return:",input);
+      setProducts(cached[input])
+      return;
     }
 
-    console.log(products);
+    const data = await axios.get(`https://dummyjson.com/products/search?q=${input}`);
 
-    useEffect(() => {
-      fetchData()
-    },[])
+    setProducts(data.data.products);
+    setcached((prev) => ({...prev, [input]:data?.data?.products}))
+  };
+
+  console.log(products);
+
+  useEffect(() => {
+
+    const timer = setTimeout(fetchData, 300 )
+
+    return () => {
+      clearTimeout(timer)
+    }  
+  }, [input]);
 
   return (
-    <div className='home'>
-        <h1 className='heading'>Welecome to Redux toolkit store</h1>
-        <div className='products'>
-                {
-                  products.map((items) => (
-                    <ProductsCard key={items.id} products={items} />
-                  ))
-                }
-        </div>
+    <div className="home">
+      <div className="main">
+        <h1 className="heading">Welecome to Redux toolkit store</h1>
+
+        <span className="search-section">
+          <input type="text" placeholder="search product here..." onChange={(e) => setInput(e.target.value)} />
+          <div className="icon"><CiSearch  /></div>
+        </span>
+      </div>
+      <div className="products">
+        {products.map((items) => (
+          <ProductsCard key={items.id} products={items} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
